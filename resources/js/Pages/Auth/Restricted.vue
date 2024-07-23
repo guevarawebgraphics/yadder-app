5c48082c15ca4d40a0b343174a4d152772f13369
ruelDev <rueljoseph.cruz@gmail.com>
2024-07-23 22:41:30 +0800

    add yadder logo

--- resources/js/Pages/Auth/Restricted.vue
+++ resources/js/Pages/Auth/Restricted.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {useForm} from '@inertiajs/vue3'
-import GuestLayout from "@/Layouts/GuestLayout.vue";
+import {Link, useForm} from '@inertiajs/vue3'
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 
 const form = useForm({
     password: null,
@@ -9,19 +9,29 @@ const form = useForm({
 </script>
 
 <template>
-    <GuestLayout>
-        <div class="">
-            <h1 class="text-4xl font-bold text-center mb-3 text-red-600">Site Restricted</h1>
-
-            <form class="flex flex-col gap-3" @submit.prevent="form.post('/restricted')">
-                <!-- email -->
-                <input type="password" v-model="form.password">
-                <small v-if="form.errors.password" class="text-red-500">{{ form.errors.password }}</small>
-
-                <button class="block rounded bg-gray-700 border p-2" type="submit" :disabled="form.processing">Verify</button>
-            </form>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div>
+            <Link href="/">
+                <ApplicationLogo class="" />
+            </Link>
         </div>
-    </GuestLayout>
+
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white border-2 border-[#F6921E] overflow-hidden rounded-2xl"
+        >
+            <div class="">
+                <h1 class="text-xl font-bold text-center mb-3 text-red-600">Site Restricted</h1>
+
+                <form class="flex flex-col gap-3" @submit.prevent="form.post('/restricted')">
+                    <!-- email -->
+                    <input type="password" v-model="form.password">
+                    <small v-if="form.errors.password" class="text-red-500">{{ form.errors.password }}</small>
+
+                    <button class="text-white block rounded bg-[#F6921E] border p-2" type="submit" :disabled="form.processing">Verify</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </template>
 
